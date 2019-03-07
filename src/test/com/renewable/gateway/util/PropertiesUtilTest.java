0069f0fc85fd9d319f60853a8e8d2ba627476f8d
jarry <15958002454@163.com>
2019-03-07 18:21:18 +0800

    底层串口通信模块重新完成主要部分。之前已经保存两次了。这次github再出莫名问题，直接换别的平台，白瞎我花的钱。

---
+++ src/test/com/renewable/gateway/util/PropertiesUtilTest.java
@@ -0,0 +1,18 @@
+package com.renewable.gateway.util;
+
+import org.junit.Test;
+
+/**
+ * @Description：
+ * @Author: jarry
+ */
+public class PropertiesUtilTest {
+
+    @Test
+    public void getProperty() {
+    }
+
+    @Test
+    public void getProperty1() {
+    }
+}
